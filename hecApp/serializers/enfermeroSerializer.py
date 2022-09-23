35af11142b467365e2522b66d7de8853b8029343
wilmar-arcila <wilmararcilac@gmail.com>
2022-09-23 17:16:42 -0500

    Actualización de serializadores

--- hecApp/serializers/enfermeroSerializer.py
+++ hecApp/serializers/enfermeroSerializer.py
@@ -1,7 +1,6 @@
 from string import printable
 from rest_framework import serializers
 from hecApp.models.enfermero import Enfermero
-from hecApp.models.usuario import Usuario
 
 class EnfermeroSerializer(serializers.ModelSerializer):
     class Meta:
--- hecApp/serializers/medicoSerializer.py
+++ hecApp/serializers/medicoSerializer.py
@@ -1,7 +1,6 @@
 from string import printable
 from rest_framework import serializers
 from hecApp.models.medico import Medico
-from hecApp.models.usuario import Usuario
 
 class MedicoSerializer(serializers.ModelSerializer):
     class Meta:
